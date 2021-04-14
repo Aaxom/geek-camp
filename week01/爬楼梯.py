0f2c3d35bbb382039f820162eef01cf02289b05b
Your Name <you@example.com>
2021-04-14 20:57:56 +0800

    爬楼梯复习和优化,添加递归模板

--- week01/爬楼梯.py
+++ week01/爬楼梯.py
@@ -1,9 +1,27 @@
+# 递归结题函数结构
+# def recursive(level, param1, param2, ...):
+#     # 递归结束条件、边界条件
+#     if level > MAX_LEVEL:
+#         process_result
+#         return
+#     # 本层处理操作
+#     process(level, data...)
+#     # 往下递归
+#     self.recursive(level+1, p1...)
+#     # 递归后清理、还原操作
+
+
 # 1. 暴力递归
 class Solution:
     def climbStairs(self, n: int) -> int:
         # 递归边界条件，当只有0或1级台阶时，只有一种爬法
         if n==0 or n==1:
             return 1
+        # 当n>1时，终止条件也可以用下面这种
+        # if n==1:
+        #     return 1
+        # elif n==2:
+        #     return 2
 
         # 爬n级台阶的方法数 = 爬n-1级台阶的方法数（这次走1步） + 爬n-2级台阶的方法数（这次走2步）
         return self.climbStairs(n-1) + self.climbStairs(n-2)
@@ -31,21 +49,22 @@ class Solution:
 class Solution:
     def climbStairs(self, n: int) -> int:
         results = [1]*(n+1)
-        for i in range(len(results)):
-            if i == 0 or i == 1:
-                results[i] == 1
-            else:
-                results[i] = results[i-1] + results[i-2]
+        for i in range(n+1): # 初始值为1情况下，可以缩短遍历范围，用range(2, n+1)
+            # 下面可以省略，因为初始化默认就是1
+            # if n==0 or n==1:
+            #     results[i] = 1
+            if i>1:
+                results[i] = results[i-1]+results[i-2]
+        # return results[-1]
+        return results[n]
 
-        return results[-1]
-
-# 4. 变量优化
+# 4. 动态规划（变量优化）
 class Solution:
     def climbStairs(self, n: int) -> int:
-        a, b = 1, 1
+        front, back = 1, 1
         for i in range(2, n+1):
-            a, b = b, a+b
-        return b
+            front, back = back, front + back
+        return back
 
 
 # 5. 斐波那契公式法
